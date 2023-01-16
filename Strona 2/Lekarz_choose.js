ad5a10f532a0989a52d38d4378b60fac303cc9a4
puechua <puechua@gmail.com>
2023-01-16 18:12:23 +0100

    chyba naprawione

---
+++ Strona 2/Lekarz_choose.js
@@ -0,0 +1,8 @@
+function chooseAM() {
+    document.getElementById("AM").innerHTML = "Antoni Markiewicz";
+}
+
+function chooseAW() {
+    document.getElementById("AW").innerHTML = "Anna Walc";
+    
+}
